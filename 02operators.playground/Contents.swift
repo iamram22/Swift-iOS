8fbbaa20fec7c05b5f047fac3c2bb1fbcc8995c9
iamram22 <ramlikestocode@gmail.com>
2024-07-17 11:55:29 +0530

    Operators

--- 02operators.playground/Contents.swift
+++ 02operators.playground/Contents.swift
@@ -22,4 +22,79 @@ print(z)
 
 "Hello, " + "iOS"
 
-//Reminder Operator
+//Reminder Operator %
+9 % 4 //prints 1
+
+//Unary minus operator
+let three = 3
+let minusThree = -three // -3
+
+//Unary plus operator
+let minusSix = -6
+let plusSix = +minusSix // -6
+
+//compound assignment operator
+var a1 = 1
+a1 += 2 //just a short-hand notation for a = a + 2
+
+//Comparison operators (returns a Boolean value, useful in conditional statements)
+1 == 1 //equals
+2 != 1 //not equals to
+2 > 1 //greater than
+1 < 2 //less than
+1 >= 1 //greater than or equal to
+1 <= 1 //less than or equal to to
+
+//tuples can also be compared
+(1, "a") < (2, "b")
+
+//conditional operators ( if...else)
+let name = "Hulk"
+if name == "Hulk" {
+    print("Hello superhero ,  Access granted")
+} else {
+    print("sorry \(name), you are not a superhero")
+}
+
+//Ternary Conditional Operators
+let iPhone15 = true
+
+iPhone15 ? "Allow updates" : "Deny Updates"
+
+//Nil-Coaliscing operator
+let myColor = "red"
+var yourColor:String?
+
+var colorToUse = yourColor ?? myColor //if yourColor is not defined myColor is used
+
+//Range operators
+//requires knowledge of loops, re-visting once done with loops
+
+//Logical operators (NOT, AND ,OR)
+//NOT operator
+let admin = false
+if !admin {
+    print("access denied, only admin can login to control-pannel")
+}
+
+//AND operator
+let adminn = true
+let subAdmin = false
+if adminn && subAdmin {
+    print("Fund transfer granted")
+} else {
+    print("admin and sub-admin both are required to be logged in to access fund transfer")
+}
+
+//OR operator
+let key = true
+let fingerPrint = false
+if key || fingerPrint {
+    print("Welcome")
+} else {
+    print("Pls use the key or your fingerprint")
+}
+
+
+
+
---
+++ 03Arrays.playground/Contents.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+var defineArray = "stores values of the same type in an ordered lis"
+// "as [Any]" after the array can be added to accept any data types in the Array
+//the same value can appear multiple times at different positions
+
+var someInts: [Int] = [] //creating an empty array
+print("someInts is of type [Int] with \(someInts) items")
+
+var shoppingCart: [String] = ["onions", "carrots"]
+
+var thingsIcannotAfford = ["ipadPro", "iPhone15Pro", "iWatch", "macbookPro16"] //Swift infers this array type as String type.
+
+//accessing and modifying an Array
+
+print("I need to buy \(shoppingCart.count) items from super market") //count property returns number of values from the array
+
+//using "isEmpty" property to check if the count os 0. "isEmpty" returns a boolean value
+if shoppingCart.isEmpty {
+    print("where is the list?")
+} else {
+    print(shoppingCart)
+}
+
+//adding new item to the end of array using "append" property
+shoppingCart.append("Milk")
+
+//adding new items to end of array using operators
+shoppingCart += ["wheat flour", "chocolates", "Coffee powder", "tea powder", "fruit jam"]
+
+
+//retrieving the values
+var firstItem = shoppingCart[0]
+
+//modifying the values of array (subscript syntax)
+shoppingCart[0] = "Eggs" //"onions" is replaced by "Eggs"
+
+//using range property to modifty values
+shoppingCart[4...6] = ["value1", "value2"] //start value is included, end value is not
+print(shoppingCart) //modification starts from index 4 and ends at 5, because end range is excluded
+
+//inserting a value at specified index
+shoppingCart.insert("detergent", at: 0) //detergent is now added to the array (doesn't replace any value)
+
+//removing values
+shoppingCart.remove(at: 0) //detergent is removed from the array
+shoppingCart
+
+shoppingCart.removeLast() //removes the last value of array
+shoppingCart
+        
+
+
+
